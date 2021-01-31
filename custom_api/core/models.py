241213889854ff41849b8b246cdeb64dc6514f39
omar115 <omarhasan115@gmail.com>
2021-01-31 17:51:55 +0600

    building serializers for database related

--- custom_api/core/models.py
+++ custom_api/core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.charField(max_length=100)
+    description = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
