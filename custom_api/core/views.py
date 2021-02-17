8820d28d27766ca2228929863f5812eddf0c3045
omar115 <omarhasan115@gmail.com>
2021-02-17 15:04:13 +0600

    building with serializers

--- custom_api/core/views.py
+++ custom_api/core/views.py
@@ -4,6 +4,9 @@ from django.http import JsonResponse, HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import PostSerializer
+from .models import Post
+
 # Create your views here.
 
 
@@ -11,11 +14,22 @@ class TestView(APIView):
     def get(self, request, *args, **kwargs):
         data = {
         'name':'John Wick',
-        'age':23,
-        'profession':'Serial Killer'
+        'Known for': 'BoogeyMan',
+        'age':32,
+        'profession':'Professional Killer',
+        'skills': 'killing in a brutal way',
+        'weak points': 'None',
 
         }
         return Response(data)
+    
+    def post(self, request, *args, **kwargs):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors)
 
 
 # def test_view(request):
